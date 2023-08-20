e3da58faec080ded921c5d6deeb29bfb6e77c16b
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-08-20 15:09:53 +0700

    Layouting login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack } from '@mui/material'
+import Login from './pages/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Stack
+    position={'fixed'}
+    width={'100vw'}
+    height={'100vh'}
+    sx={{
+      background: '#f5f5f5'
+    }}
+  >
+    <Login/>
+  </Stack>
 }
-
-export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import { Button, Stack, TextField } from '@mui/material'
+import { ChangeEvent, useState } from 'react'
+import { LoginReq } from '../types/user/LoginReq'
+
+export default function Login() {
+    const [loginReq, setLoginReq] = useState<LoginReq>({
+        username: '',
+        password: ''
+    })
+
+    return <Stack
+        position={'absolute'}
+        top={'50%'}
+        left={'50%'}
+        width={'20vw'}
+        sx={{
+            transform: 'translate(-50%, -50%)'
+        }}
+    >
+        <center>
+            <img src='logo.png' alt='Logo PTPNX' width={'70%'} />
+        </center>
+        <TextField
+            variant={'outlined'}
+            size={'small'}
+            label={'Username'}
+            value={loginReq.username}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                setLoginReq({
+                    ...loginReq,
+                    username: event.target.value
+                })
+            }}
+            sx={{
+                mb: 2
+            }}
+        />
+        <TextField
+            variant={'outlined'}
+            size={'small'}
+            label={'Password'}
+            type={'password'}
+            value={loginReq.password}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                setLoginReq({
+                    ...loginReq,
+                    password: event.target.value
+                })
+            }}
+            sx={{
+                mb: 2
+            }}
+        />
+        <Button
+            variant={'contained'}
+            color={'primary'}
+            onClick={() => console.log(loginReq)}
+        >
+            Login
+        </Button>
+    </Stack>
+}

9384dfb1199b5ff7fe347ffa6ee0776ecc4fffbf
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-08-20 15:09:31 +0700

    Create login entity

---
+++ src/types/user/LoginResp.ts
@@ -0,0 +1,10 @@
+import { BaseResp } from '../BaseResp'
+import { User } from './User'
+
+export interface LoginResp extends BaseResp {
+    outputSchema: OutputSchema
+}
+
+export interface OutputSchema extends User {
+    token: string
+}

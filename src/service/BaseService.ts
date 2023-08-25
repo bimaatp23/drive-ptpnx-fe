201ff92e9034eb178fba78c0d1e047ac129ae11f
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-08-25 16:07:23 +0700

    Update base service

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -3,10 +3,13 @@ import { UseCaseFactory, UseCaseFactoryImpl } from '../usecase/UseCaseFactory'
 
 export interface BaseService {
     httpGet(url: string, config?: AxiosRequestConfig<any>): Promise<AxiosResponse<any, any>>
+    httpPost(url: string, data?: any, config?: AxiosRequestConfig<any>): Promise<AxiosResponse<any, any>>
 }
 
 export class BaseServiceImpl implements BaseService {
     useCaseFactory: UseCaseFactory = new UseCaseFactoryImpl()
+
+    readonly endPoint = 'http://localhost:3001/drive/api'
 
     httpGet(url: string, config?: AxiosRequestConfig<any>): Promise<AxiosResponse<any, any>> {
         const defaultConfig: AxiosRequestConfig<any> = {
@@ -15,6 +18,16 @@ export class BaseServiceImpl implements BaseService {
                 Authorization: 'Bearer ' + this.useCaseFactory.createSessionUseCase().getToken()
             }
         }
-        return axios.get(url, defaultConfig)
+        return axios.get(this.endPoint + url, defaultConfig)
+    }
+
+    httpPost(url: string, data?: any, config?: AxiosRequestConfig<any> | undefined): Promise<AxiosResponse<any, any>> {
+        const defaultConfig: AxiosRequestConfig<any> = {
+            ...config,
+            headers: {
+                Authorization: 'Bearer ' + this.useCaseFactory.createSessionUseCase().getToken()
+            }
+        }
+        return axios.post(this.endPoint + url, data, defaultConfig)
     }
 }

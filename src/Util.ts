b42c7555e5e9d40740b423ce2069132387f8c87e
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-12-07 11:51:43 +0700

    Create util set notification

---
+++ src/Util.ts
@@ -0,0 +1,16 @@
+import Swal from "sweetalert2"
+
+interface setNotificationProps {
+    icon: "success" | "error" | "warning" | "info" | "question"
+    message: string
+}
+
+export function setNotification(props: setNotificationProps) {
+    const { icon, message } = props
+    Swal.fire({
+        title: message,
+        icon: icon,
+        showConfirmButton: false,
+        timer: 3000
+    })
+}

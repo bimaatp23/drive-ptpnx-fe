8accd1cc607467ec819db2b26bb4eac0a5ea8e0a
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-12-07 11:52:17 +0700

    Create pdf viewer

---
+++ src/components/PDFViewer.tsx
@@ -0,0 +1,28 @@
+import { Viewer, Worker } from "@react-pdf-viewer/core"
+import "@react-pdf-viewer/core/lib/styles/index.css"
+import { useEffect, useState } from "react"
+
+interface Props {
+    file: File
+}
+
+export default function PDFViewer(props: Props) {
+    const [fileUrl, setFileUrl] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        const reader = new FileReader()
+
+        reader.onloadend = () => {
+            const result = reader.result
+            if (typeof result === "string") {
+                setFileUrl(result)
+            }
+        }
+
+        reader.readAsDataURL(props.file)
+    }, [props.file])
+
+    return <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        {fileUrl ? <Viewer fileUrl={fileUrl} /> : <></>}
+    </Worker>
+}

27f7ebfd37f8eb268e1dd793286642b1f91a79f1
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-12-20 22:37:25 +0700

    Delete basic constant

--- src/types/BasicConstant.ts
+++
@@ -1,6 +0,0 @@
-export default class BasicConstant {
-    public static KATEGORI_KASBON = "kasbon"
-    public static KATEGORI_MEMORIAL = "memorial"
-    public static KATEGORI_NERACA = "neraca"
-    public static KATEGORI_UMUM = "umum"
-}

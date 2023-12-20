b1129a5442b01bb2136242c513c0f20f3261ad12
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-12-20 20:46:07 +0700

    Consume add category use case

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,14 +1,22 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-import { useEffect, useMemo, useState } from "react"
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material"
+import { ChangeEvent, KeyboardEvent, useEffect, useMemo, useState } from "react"
+import { finalize } from "rxjs"
 import { setNotification } from "../Util"
+import CustomModal from "../components/CustomModal"
 import MainPage from "../components/MainPage"
+import { BaseResp } from "../types/BaseResp"
 import { Category as CategoryType } from "../types/category/Category"
-import { GetCatgeorysResp } from "../types/category/GetCategorysResp"
+import { CreateCategoryReq } from "../types/category/CreateCategoryReq"
+import { GetCategorysResp } from "../types/category/GetCategorysResp"
 import { UseCaseFactory, UseCaseFactoryImpl } from "../usecase/UseCaseFactory"
 
 export default function Category() {
     const useCaseFactory: UseCaseFactory = useMemo(() => new UseCaseFactoryImpl(), [])
     const [categorys, setCategorys] = useState<CategoryType[]>([])
+    const [createCategoryReq, setCreateCategoryReq] = useState<CreateCategoryReq>({
+        name: ""
+    })
+    const [isModalCreateOpen, setIsModalCreateOpen] = useState<boolean>(false)
 
     const [isStatic, setIsStatic] = useState<boolean>(false)
     useEffect(() => setIsStatic(true), [])
@@ -17,7 +25,7 @@ export default function Category() {
         if (isStatic) {
             useCaseFactory.useGetCategorysUseCase().execute()
                 .subscribe({
-                    next: (response: GetCatgeorysResp) => {
+                    next: (response: GetCategorysResp) => {
                         if (response.errorSchema.errorCode === 200) {
                             setCategorys(response.outputSchema)
                         }
@@ -32,9 +40,101 @@ export default function Category() {
         }
     }, [isStatic, useCaseFactory])
 
+    const getCategoryList = (): void => {
+        useCaseFactory.useGetCategorysUseCase().execute()
+            .subscribe({
+                next: (response: GetCategorysResp) => {
+                    if (response.errorSchema.errorCode === 200) {
+                        setCategorys(response.outputSchema)
+                    }
+                },
+                error: (error) => {
+                    setNotification({
+                        icon: "error",
+                        message: error.response.data.errorSchema?.errorMessage ?? error.response.statusText
+                    })
+                }
+            })
+    }
+
+    const doCreateCategory = (): void => {
+        useCaseFactory.useCreateCategoryUseCase().execute(createCategoryReq)
+            .pipe(
+                finalize(() => {
+                    setIsModalCreateOpen(false)
+                    getCategoryList()
+                })
+            )
+            .subscribe({
+                next: (response: BaseResp) => {
+                    if (response.errorSchema.errorCode === 200) {
+                        setNotification({
+                            icon: "success",
+                            message: response.errorSchema.errorMessage
+                        })
+                        setCreateCategoryReq({
+                            name: ""
+                        })
+                    }
+                },
+                error: (error) => {
+                    setNotification({
+                        icon: "error",
+                        message: error.response.data.errorSchema?.errorMessage ?? error.response.statusText
+                    })
+                }
+            })
+    }
+
+    const displayCreateModal = () => {
+        return <CustomModal
+            title="Tambah Loker"
+            open={isModalCreateOpen}
+            onClose={() => setIsModalCreateOpen(false)}
+        >
+            <TextField
+                size="small"
+                id="name"
+                label="Nama"
+                fullWidth
+                value={createCategoryReq.name}
+                onChange={handleCreateOnChange}
+                onKeyDown={handleCreateOnEnter}
+            />
+            <Button
+                variant="contained"
+                onClick={doCreateCategory}
+            >
+                Tambah
+            </Button>
+        </CustomModal>
+    }
+
+    const handleCreateOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setCreateCategoryReq({
+            ...createCategoryReq,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    const handleCreateOnEnter = (e: KeyboardEvent<HTMLDivElement>): void => {
+        if (e.key === "Enter") {
+            doCreateCategory()
+        }
+    }
+
     return <MainPage
         title="Kategori"
+        headElement={
+            <Button
+                variant="contained"
+                onClick={() => setIsModalCreateOpen(true)}
+            >
+                Tambah Kategori
+            </Button>
+        }
     >
+        {displayCreateModal()}
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
